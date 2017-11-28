4156d6e46ab24f62053ab3bcb6071cdd016646a2
Joseph Hassell <josephh2018@gmail.com>
2017-11-28 17:06:26 -0600

    Motors Enum

--- include/config.h
+++ include/config.h
@@ -2,8 +2,15 @@
 #define _CONFIG_H_
 
 enum Ports{
-  left_motor = 1,
-  right_motor = 10
+  insta_left = 1,         //InstaL
+  left_motor = 2,         // Left
+  right_motor = 3,        // Right
+  claw = 5,               // Claw
+  right_arm = 6,          // RaiseR
+  left_arm = 7,           // RaiseL
+  right_lift = 8,         // Lift1_R
+  left_lift = 9,          // Lift1_L
+  insta_right = 10        // InstaR
 };
 
 
