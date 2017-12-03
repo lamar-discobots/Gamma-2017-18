d65db604996876c4edfb89908ce53244ff812523
Alaric Hunziker <alaric.hunz@gmail.com>
2017-12-02 21:38:16 -0600

    Arcade drive that doesn't work

--- src/opcontrol.c
+++ src/opcontrol.c
@@ -29,8 +29,21 @@
  *
  * This task should never exit; it should end with some kind of infinite loop, even if empty.
  */
+
+ int joystickGetAnalog ( unsigned char joystick,   // the joystick slot to check (1 for master, 2 for partner)
+                         unsigned char axis        // One of the joystick channels on a VEX Joystick: 1, 2, 3, 4, ACCEL_X, or ACCEL_Y
+                       );
+void motorSet ( unsigned char channel,  // motor channel to set from 1-10
+											   int speed               // new signed speed. -127 is full reverse, 127 full forward, 0 off
+											 );
 void operatorControl() {
-	while (1) {
-		delay(20);
-	}
+	int power;
+	int turn;
+		while (1) {
+			power = joystickGetAnalog(1, 2); // vertical axis on right joystick
+			turn  = joystickGetAnalog(1, 1); // horizontal axis on right joystick
+			motorSet(2, power + turn); // set left wheels
+			motorSet(3, power - turn); // set right wheels
+			delay(20);
+}
 }
