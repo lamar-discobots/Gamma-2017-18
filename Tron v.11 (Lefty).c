03359fc9cf2d5ef8d46303733606e331cecf0c24
Alaric Hunziker <alaric.hunz@gmail.com>
2017-12-23 17:03:27 -0600

    Made the robot turn 235 degrees in the left program

--- Tron v.11 (Lefty).c
+++ Tron v.11 (Lefty).c
@@ -136,14 +136,14 @@ task autonomous()
 
 	resetMotorEncoder(Left);
 	resetMotorEncoder(Right);
-	while(abs(getMotorEncoder(Right)) < getRotationGoal(200))
+	while(abs(getMotorEncoder(Right)) < getRotationGoal(235))
 	{
 		motor[Left]= -127;
 		motor[Right]= 127;
 	}
 	motor[Left]= 0;
 	motor[Right]= 0;
-// The robot turns 200 degrees
+// The robot turns 235 degrees
 
 	resetMotorEncoder(Left);
 	resetMotorEncoder(Right);
