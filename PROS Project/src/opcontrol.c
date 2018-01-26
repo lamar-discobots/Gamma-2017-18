63efcebfb02e078776c206112e0dddbb137d8589
Unknown <alaric.hunz@gmail.com>
2018-01-26 12:57:13 -0600

    Gonna work on hardcore PROS programming over the weekend

---
+++ PROS Project/src/opcontrol.c
@@ -0,0 +1,9 @@
+#include "main.h"
+int joystickGetAnalog ( unsigned char joystick, unsigned char axis);
+void motorSet ( unsigned char channel, int speed);
+
+void operatorControl() {
+	while (1) {
+		delay(20);
+	}
+}
