908543121741b1cf7259879b256e83b30319907e
Unknown <alaric.hunz@gmail.com>
2018-01-29 12:55:23 -0600

    Messed up ports today

--- PROS Project/src/opcontrol.c
+++ PROS Project/src/opcontrol.c
@@ -3,7 +3,13 @@ int joystickGetAnalog ( unsigned char joystick, unsigned char axis);
 void motorSet ( unsigned char channel, int speed);
 
 void operatorControl() {
-	while (1) {
-		delay(20);
-	}
+  int power;
+  int turn;
+    while (1) {
+        power = joystickGetAnalog(1, 2); // vertical axis on left joystick
+        turn  = joystickGetAnalog(1, 1); // horizontal axis on left joystick
+        motorSet(2, power + turn); // set left wheels
+        motorSet(3, power - turn); // set right wheels
+        delay(20);
+    }
 }
